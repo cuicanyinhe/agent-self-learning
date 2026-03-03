bac32cbb70f8ec19e60d2d8de00d98b734d95192
cuicanyinhe <2947526604@qq.com>
2026-03-03 23:46:14 +0800

    first commit

---
+++ utils/prompts_loader.py
@@ -0,0 +1,45 @@
+from utils.config_handler import prompts_conf
+from utils.logger_handler import logger
+from utils.path_tool import get_abs_path
+
+
+def load_system_prompts():
+    try:
+        system_prompt_path = get_abs_path(prompts_conf["main_prompt_path"])
+    except KeyError as e:
+        logger.error(f"[load_system_prompts]在yaml配置项中没有main_prompt_path配置项")
+        raise e
+
+    try:
+        return open(system_prompt_path, "r", encoding="utf-8").read()
+    except Exception as e:
+        logger.error(f"[load_system_prompts]在解释提示词系统时出错，{str((e))}")
+        raise e
+
+
+def load_rag_prompts():
+    try:
+        rag_prompt_path = get_abs_path(prompts_conf["rag_summarize_prompt_path"])
+    except KeyError as e:
+        logger.error(f"[load_rag_prompts]在yaml配置项中没有rag_summarize_prompt_path配置项")
+        raise e
+
+    try:
+        return open(rag_prompt_path, "r", encoding="utf-8").read()
+    except Exception as e:
+        logger.error(f"[load_rag_prompts]在解释RAG总结提示出错，{str((e))}")
+        raise e
+
+
+def load_report_prompts():
+    try:
+        report_prompt_path = get_abs_path(prompts_conf["report_prompt_path"])
+    except KeyError as e:
+        logger.error(f"[load_report_prompts]在yaml配置项中没有report_prompt_path配置项")
+        raise e
+
+    try:
+        return open(report_prompt_path, "r", encoding="utf-8").read()
+    except Exception as e:
+        logger.error(f"[load_report_prompts]在解释报告生成提示词出错，{str((e))}")
+        raise e
