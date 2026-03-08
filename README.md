f2bbb43d679e39118477485b3b70a99a0b98825e
cuicanyinhe <2947526604@qq.com>
2026-03-08 22:40:17 +0800

    apdate README

--- README.md
+++ README.md
@@ -1,18 +1,4 @@
-##一个agent开发的初学者
-尝试自己调用api后对模型提问，尝试使用github创建分支然后commit push到自己仓库，再自己pull request,merge到main
+一个智能客服机器人，可以与用户交互问答，并生成使用报告
 
-reflog查看所有操作历史 reset重置到该操作
+启动方式： streamlit run app.py
 
-
-
-草草的写好了tools
-后续准备接入mcp和修改部分库的调用（尤其是langchain
-
-
-改成一个游戏秘书管家或者别的，多调用免费写好的tools
-比如杀戮尖塔管家
-
-爬虫爬wiki上尖塔的内容
-有没有机会复合成多模态agent,聊天游戏工具等多合一
-
-mcp的接入非常困难，无从下手(已完成)
--- agent/tools/agent_tools.py
+++ agent/tools/agent_tools.py
@@ -17,21 +17,21 @@ external_data = {}
 def rag_summarize(query: str) -> str:
     return rag.rag_summarize(query)
 
-@tool(description="获取指定城市的天气，以消息字符串的形式返回")
+'''@tool(description="获取指定城市的天气，以消息字符串的形式返回")
 def get_weather(city: str) -> str:
     return f"城市{city}天气为晴天，气温26摄氏度，空气湿度50%，南风1级，AQI21，最近6小时降雨概率极低"
 
 @tool(description="获取用户所在城市的名称，以纯字符串形式返回")
 def get_user_location() -> str:
-    return random.choice(["深圳", "合肥", "杭州"])
+    return random.choice(["深圳", "合肥", "杭州"])'''
 
 @tool(description="获取用户的ID，以纯字符串形式返回")
 def get_user_id() -> str:
     return random.choice(user_ids)
 
-@tool(description="获取当前月份，以纯字符串形式返回")
+'''@tool(description="获取当前月份，以纯字符串形式返回")
 def get_current_month() -> str:
-    return random.choice(month_arr)
+    return random.choice(month_arr)'''
 
 def generate_external_data():
     if not external_data:
